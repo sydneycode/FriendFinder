eff442d8fbbec8b08e7375bae910185cdd6536ad
sydneycode <sstaggs12@gmail.com>
2018-09-25 01:04:28 -0700

    ensured that the API POST request was working correctly

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
 
     // API POST Request
     // A POST route with the url '/api/friends', which will be used to 
-    // handle incoming survey reuslts; this route will also be used to
+    // handle incoming survey results; this route will also be used to
     // handle the compatibility logic
     app.post("/api/friends", function(req, res) {
         console.log(req.body);
@@ -32,13 +32,15 @@ module.exports = function(app) {
                 mostCompatibleFriend = friendsData[i];
             }
         }
+        console.log("Name: " + newUser.name + " | Photo: " + newUser.photo);
 
         // Add the new user's data to the array of objects representing a list of friends
         friendsData.push(newUser);
+        res.json(newUser);
 
         // Display the new user's most compatible friend, using a modal pop-up that 
         // displays both the name and picture of the closest match
-        console.log("Name: " + newUser.name + " | Photo: " + newUser.photo);
+        console.log("Name: " + mostCompatibleFriend.name + " | Photo: " + mostCompatibleFriend.photo);
     });
 
 };
